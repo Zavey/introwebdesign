5f8ef449dcb1b56d8e86e148218c4b3177821dac
xavier <xavierc97@gmail.com>
2019-04-30 10:24:05 -0400

    updates

--- e17/js/planet.js
+++
@@ -1,8 +0,0 @@
-var imagesArray = ["earth.jpg", "jupiter.jpg", "mars.jpg", "neptune.jpeg", "saturn.png"];
-
-
-    function displayImage(){
-
-    var num = Math.floor(Math.random() * 7); // 0...6
-    document.canvas.src = imagesArray[num];
-}
